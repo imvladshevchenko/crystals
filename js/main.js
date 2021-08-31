2caef307274a5bc3e465a10065024f8f10b0b8b2
Vladyslav Shevchenko <imvladshevchenko@gmail.com>
2021-09-01 01:38:25 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+AOS.init();
+
+// let crystal = document.getElementById('test')
+
+// function disappear() {
+//   let isStarted = false
+//   if (isStarted === false) {
+//     isStarted = true
+//     setTimeout(zIndexUnder, 900)
+//   }
+//   if (isStarted) {
+//     setInterval(() => {
+//       zIndexAbove()
+//     }, 2500);
+//   }
+//   if (isStarted) {
+//     setInterval(() => {
+//       zIndexUnder()
+//     }, 10500);
+//   }
+// }
+
+// function zIndexUnder() {
+//   crystal.style.zIndex = "10"
+// }
+// function zIndexAbove() {
+//   crystal.style.zIndex = "25"
+// }
+
+// disappear()
